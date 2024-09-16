826edff07ae51bdcd03100133d7ed754ad6e7370
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2024-09-16 21:57:47 +0530

    Done some name changes.

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -56,7 +56,7 @@ function SignIn() {
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
         <div className="text-center">
           <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-            Enter into True Feedback
+            Enter into Mystery Messenger
           </h1>
           <p className="mb-4">Sign in to start your anonymous adventure</p>
         </div>
